b45120752f197092e1840d5a41c212aa813872ad
jerry92k <jens92.k@gmail.com>
2021-10-03 21:41:56 +0900

    feat(Application.java): 게임 흐름 제어 기능 정의

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,65 @@
 package baseball;
 
+import baseball.domain.NumberQuiz;
+import baseball.domain.QuizResult;
+import baseball.exception.InvalidInputCommandException;
+import nextstep.utils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 숫자 야구 게임 구현
+        Application application=new Application();
+        application.startGame();
+    }
+
+    public void startGame() {
+        do{
+            gameRoutine();
+        }while (isContinueGame());
+    }
+
+    public boolean isContinueGame() {
+        try {
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String input = Console.readLine();
+            validateGameStateCommand(input);
+            return input.equals("1") ? true : false;
+        }
+        catch (InvalidInputCommandException ex){
+            System.out.println(ex.getMessage());
+            return isContinueGame();
+        }
+    }
+
+    public void gameRoutine(){
+
+        NumberQuiz numberQuiz=new NumberQuiz();
+        numberQuiz.produceRandomQuiz();
+        while (!tryMatchNumber(numberQuiz)){
+        }
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 끝");
+    }
+
+    public void validateGameStateCommand(String input) {
+        if(input==null){
+            throw new InvalidInputCommandException("명령이 입력되지 않았습니다.");
+        }
+        if(input.equals("1")||input.equals("2")){
+            return;
+        }
+        throw new InvalidInputCommandException("올바른 값이 입력되지 않았습니다.");
+    }
+
+    public boolean tryMatchNumber(NumberQuiz numberQuiz){
+        try {
+            System.out.print("숫자를 입력해주세요 : ");
+            QuizResult quizResult = numberQuiz.solveQuiz(Console.readLine());
+            System.out.println(quizResult.getHint());
+            return quizResult.isSolved();
+        }
+        catch (InvalidInputCommandException ex){
+            System.out.println(ex.getMessage());
+            return false;
+        }
     }
 }
